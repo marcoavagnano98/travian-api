9694e721b084518254cc9e1c59e8d134d243a8e5
Marco Avagnano <marco.avagnano@studio.unibo.it>
2022-12-28 23:42:31 +0100

    add features

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 if __name__ == '__main__':
     from account import Account
 
-    #account = Account("https://ts3.x1.america.travian.com/", {"name": "pomiy11146", "password": "americanpie"})
-
-    account = Account("https://ts4.x1.europe.travian.com/", {"name": "mark98", "password": "tirana"})
+    account = Account("https://ts3.x1.america.travian.com/", {"name": "pomiy11146", "password": "americanpie"})
     account_info = account.get_all_buildings()
     a = 0
 
