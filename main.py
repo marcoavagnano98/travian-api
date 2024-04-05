30df16be87e2a41d5722d3bacea545e1f5cdeb53
Marco Avagnano <marcoavagnano98@gmail.com>
2024-04-05 11:02:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 if __name__ == '__main__':
     from account import Account
-
-    account = Account("https://ts3.x1.america.travian.com/", {"name": "your_username", "password": "your_password"})
+    server_url = "https://ts3.x1.america.travian.com/"
+    account = Account(server_url, {"name": "your_username", "password": "your_password"})
     account_info = account.get_all_buildings()
-    a = 0
-
-
